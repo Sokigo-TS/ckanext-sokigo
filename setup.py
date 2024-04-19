a3064220cf090d89c6a3a86407e9eab5cd78942f
sokigo\vijo <vivek.joshi@sokigo.com>
2024-04-19 10:53:03 +0200

    added changes to include webassets.yml file

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # have to be included in MANIFEST.in as well.
     include_package_data=True,
     package_data={
-        'ckanext': ['sokigo/public/geodirekt.png']
+        'ckanext': ['sokigo/public/geodirekt.png', 'sokigo/fanstatic/common.js', 'sokigo/fanstatic/webassets.yml']
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
